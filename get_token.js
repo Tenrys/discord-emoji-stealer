87cdf8192212b9847cd7da522f0a82e28a752977
Marceau Maubert <tenrys.iaido@live.fr>
2018-07-08 10:22:21 +0200

    Clean up code a bit

--- get_token.js
+++ get_token.js
@@ -15,7 +15,7 @@ let appData = process.env.APPDATA || (process.platform == "darwin" ? process.env
 async function getToken() {
     // Find process name so we know what directory to peek inside in AppData
     console.log("Looking for Discord process...")
-    let name = "discord"
+    let name
     if (!name) {
         await (new Promise((resolve) => { ps.lookup({
             command: "discord",
@@ -27,37 +27,43 @@ async function getToken() {
             for (const res of resList) {
                 if (res) {
                     name = path.basename(res.command, path.extname(res.command)).toLowerCase()
+
+                    console.log(`Found ${name}!`)
                     resolve()
                     break
                 }
             }
         })}))
     }
-    console.log(`Found ${name}!`)
 
-    // Copy the local storage so we can read it
     console.log("Copying application's Local Storage to temporary file...")
     let dbPath = path.join(appData, name, "Local Storage", "https_discordapp.com_0.localstorage")
-    fs.copyFileSync(dbPath, "./localstorage.tmp")
-    dbPath = "./localstorage.tmp"
-    console.log("Success!")
+    fs.copyFile(dbPath, "./localstorage.tmp", (err) => {
+        if (err) {
+            throw new Error(err)
+        }
 
-    // Grab the token
+        dbPath = "./localstorage.tmp"
+
+        console.log("Success!")
+    })
+
     console.log("Connecting to Local Storage SQLite database...")
-    let db = await sqlite.open(dbPath)
-    console.log("Connected!")
-    let token = await db.get("SELECT value FROM ItemTable WHERE key = 'token'")
-        .then((res) => {
-            return /^"(.*)"$/gi.exec(res.value.toString("utf16le"))[1]
-        })
-    console.log("Got token!")
+    let db = await sqlite.open(dbPath).then(() => console.log("Connected!"))
+    let token = await db.get("SELECT value FROM ItemTable WHERE key = 'token'").then((res) => {
+        let token = /^"(.*)"$/gi.exec(res.value.toString("utf16le"))[1]
 
-    // Clean up the local storage copy
+        console.log("Got token!")
+        return token
+    })
+
     console.log("Cleaning up temporary file...")
-    await db.close().then(() => fs.unlinkSync(dbPath))
-    console.log("Success!")
+    await db.close().then(() => {
+        fs.unlinkSync(dbPath)
 
-    // Return token
+        console.log("Success!")
+    })
+
     return token
 }
 
